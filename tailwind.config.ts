f7cd6d1b518e9cea8096a612d8592f14c039f7bb
Bhargav Sah <bhaskarsah878@gmail.com>
2025-08-28 05:23:07 +0000

    check the topography of website its useless

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
